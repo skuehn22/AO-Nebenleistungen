c6cbbfa84f082c27d5e30c268349ff1c07f57894
skuehn22 <kuehn.sebastian@gmail.com>
2014-07-28 17:14:35 +0200

    delete tmp

--- public/tmp/tpl/Front_Programmstart_Buchungstyp.php
+++
@@ -1,6 +0,0 @@
-<?php if(!class_exists('raintpl')){exit;}?><?php if( $value["buchungstyp"] == 1 ){ ?>
-    <img src="/buttons/t.png"> <?php echo translate('Testphase: '); ?>
-    <?php echo translate('vorerst nur als Reservierungswunsch buchbar.'); ?>
-    <br>
-<?php } ?>
-&nbsp;
